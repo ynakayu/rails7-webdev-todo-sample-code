495ec84f35fb19e0b1e24b44157e77f353354138
ynakayu <9111754+ynakayu@users.noreply.github.com>
2022-01-14 05:18:05 +0000

    S21a-05-edit-migration-file

--- db/migrate/20220114045556_create_tasks.rb
+++ db/migrate/20220114045556_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.boolean :completed
+      t.boolean :completed, default: false
 
       t.timestamps
     end
