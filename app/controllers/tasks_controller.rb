e384ac2ca2b60fe1d339e138abeb445a2f2a5f0a
ynakayu <9111754+ynakayu@users.noreply.github.com>
2022-01-17 03:53:36 +0000

    S21a-14-add-validate

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,8 +9,11 @@ class TasksController < ApplicationController
   
   def create
     @task = Task.new(task_params)
-    @task.save
-    redirect_to root_path
+    if @task.save
+      redirect_to root_path
+    else
+      render 'new', status: :unprocessable_entity
+    end
   end
   
   private
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,3 @@
 class Task < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 5}
 end
